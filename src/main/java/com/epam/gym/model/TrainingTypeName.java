f33abe5080e484f3dba3853292a2582c1c100c3d
Mishanya <ribacokmisa@gmail.com>
2026-06-19 14:37:25 +0300

    feat: add domain model (User, Trainer, Trainee, Training, TrainingType)

---
+++ src/main/java/com/epam/gym/model/TrainingTypeName.java
@@ -0,0 +1,13 @@
+package com.epam.gym.model;
+
+/**
+ * Enumeration of available training type names.
+ */
+public enum TrainingTypeName {
+    FITNESS,
+    YOGA,
+    CARDIO,
+    STRENGTH,
+    CROSSFIT,
+    PILATES
+}
