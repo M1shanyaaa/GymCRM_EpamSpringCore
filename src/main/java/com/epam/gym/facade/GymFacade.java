003a141982fe5a9c3abe47eab141af1f8c4ec186
Mishanya <ribacokmisa@gmail.com>
2026-07-04 14:39:56 +0300

    feat: add TrainingDao with dynamic criteria queries

--- src/main/java/com/epam/gym/facade/GymFacade.java
+++ src/main/java/com/epam/gym/facade/GymFacade.java
@@ -3,6 +3,7 @@ package com.epam.gym.facade;
 import com.epam.gym.model.Trainee;
 import com.epam.gym.model.Trainer;
 import com.epam.gym.model.Training;
+import com.epam.gym.model.TrainingTypeName;
 import com.epam.gym.service.TraineeService;
 import com.epam.gym.service.TrainerService;
 import com.epam.gym.service.TrainingService;
@@ -11,7 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Facade providing a single entry point to the Gym CRM subsystem.
@@ -39,67 +42,102 @@ public class GymFacade {
 
     // ===================== Trainee =====================
 
-    public Trainee createTrainee(Trainee trainee) {
+    public Trainee createTrainee(String firstName, String lastName,
+                                 LocalDate dateOfBirth, String address) {
         log.debug("Facade: createTrainee");
-        return traineeService.create(trainee);
+        return traineeService.create(firstName, lastName, dateOfBirth, address);
     }
 
-    public Trainee updateTrainee(Trainee trainee) {
-        log.debug("Facade: updateTrainee");
-        return traineeService.update(trainee);
+    public Trainee getTrainee(String username, String password) {
+        log.debug("Facade: getTrainee username={}", username);
+        return traineeService.findByUsername(username, password);
     }
 
-    public void deleteTrainee(Long id) {
-        log.debug("Facade: deleteTrainee id={}", id);
-        traineeService.delete(id);
+    public Trainee updateTrainee(String username, String password,
+                                 String firstName, String lastName,
+                                 LocalDate dateOfBirth, String address) {
+        log.debug("Facade: updateTrainee username={}", username);
+        return traineeService.update(username, password, firstName, lastName, dateOfBirth, address);
     }
 
-    public Trainee getTrainee(Long id) {
-        log.debug("Facade: getTrainee id={}", id);
-        return traineeService.select(id);
+    public void changeTraineePassword(String username, String oldPassword, String newPassword) {
+        log.debug("Facade: changeTraineePassword username={}", username);
+        traineeService.changePassword(username, oldPassword, newPassword);
     }
 
-    public List<Trainee> getAllTrainees() {
-        log.debug("Facade: getAllTrainees");
-        return traineeService.selectAll();
+    public void toggleTraineeActive(String username, String password) {
+        log.debug("Facade: toggleTraineeActive username={}", username);
+        traineeService.toggleActive(username, password);
+    }
+
+    public void deleteTrainee(String username, String password) {
+        log.debug("Facade: deleteTrainee username={}", username);
+        traineeService.delete(username, password);
     }
 
     // ===================== Trainer =====================
 
-    public Trainer createTrainer(Trainer trainer) {
+    public Trainer createTrainer(String firstName, String lastName,
+                                 TrainingTypeName specialization) {
         log.debug("Facade: createTrainer");
-        return trainerService.create(trainer);
+        return trainerService.create(firstName, lastName, specialization);
     }
 
-    public Trainer updateTrainer(Trainer trainer) {
-        log.debug("Facade: updateTrainer");
-        return trainerService.update(trainer);
+    public Trainer getTrainer(String username, String password) {
+        log.debug("Facade: getTrainer username={}", username);
+        return trainerService.findByUsername(username, password);
     }
 
-    public Trainer getTrainer(Long id) {
-        log.debug("Facade: getTrainer id={}", id);
-        return trainerService.select(id);
+    public Trainer updateTrainer(String username, String password,
+                                 String firstName, String lastName,
+                                 TrainingTypeName specialization) {
+        log.debug("Facade: updateTrainer username={}", username);
+        return trainerService.update(username, password, firstName, lastName, specialization);
     }
 
-    public List<Trainer> getAllTrainers() {
-        log.debug("Facade: getAllTrainers");
-        return trainerService.selectAll();
+    public void changeTrainerPassword(String username, String oldPassword, String newPassword) {
+        log.debug("Facade: changeTrainerPassword username={}", username);
+        trainerService.changePassword(username, oldPassword, newPassword);
+    }
+
+    public void toggleTrainerActive(String username, String password) {
+        log.debug("Facade: toggleTrainerActive username={}", username);
+        trainerService.toggleActive(username, password);
+    }
+
+    public List<Trainer> getUnassignedTrainers(String traineeUsername, String password) {
+        log.debug("Facade: getUnassignedTrainers traineeUsername={}", traineeUsername);
+        return trainerService.findUnassignedTrainers(traineeUsername, password);
     }
 
     // ===================== Training =====================
 
-    public Training createTraining(Training training) {
-        log.debug("Facade: createTraining");
-        return trainingService.create(training);
+    public Training addTraining(String callerUsername, String callerPassword,
+                                String traineeUsername, String trainerUsername,
+                                String trainingName, LocalDate trainingDate,
+                                Integer trainingDuration) {
+        log.debug("Facade: addTraining name={}", trainingName);
+        return trainingService.addTraining(callerUsername, callerPassword,
+                traineeUsername, trainerUsername, trainingName, trainingDate, trainingDuration);
     }
 
-    public Training getTraining(Long id) {
-        log.debug("Facade: getTraining id={}", id);
-        return trainingService.select(id);
+    public List<Training> getTraineeTrainings(String username, String password,
+                                              LocalDate fromDate, LocalDate toDate,
+                                              String trainerName, TrainingTypeName trainingType) {
+        log.debug("Facade: getTraineeTrainings username={}", username);
+        return trainingService.getTraineeTrainings(username, password, fromDate, toDate, trainerName, trainingType);
     }
 
-    public List<Training> getAllTrainings() {
-        log.debug("Facade: getAllTrainings");
-        return trainingService.selectAll();
+    public List<Training> getTrainerTrainings(String username, String password,
+                                              LocalDate fromDate, LocalDate toDate,
+                                              String traineeName) {
+        log.debug("Facade: getTrainerTrainings username={}", username);
+        return trainingService.getTrainerTrainings(username, password, fromDate, toDate, traineeName);
+    }
+
+    public Set<Trainer> updateTraineeTrainers(String traineeUsername, String password,
+                                              List<String> trainerUsernames) {
+        log.debug("Facade: updateTraineeTrainers traineeUsername={}", traineeUsername);
+        return trainingService.updateTraineeTrainers(traineeUsername, password, trainerUsernames);
     }
 }
