237229355ebce71cc83b06ee20fc7d8871593c8f
Mishanya <ribacokmisa@gmail.com>
2026-07-25 12:25:42 +0300

    del: deleting unnecessary files

--- src/main/java/com/epam/gym/dto/Credentials.java
+++
@@ -1,8 +0,0 @@
-package com.epam.gym.dto;
-
-/**
- * Caller credentials extracted from request headers
- * Will be replaced by Spring Security principal later.
- */
-public record Credentials(String username, String password) {
-}
