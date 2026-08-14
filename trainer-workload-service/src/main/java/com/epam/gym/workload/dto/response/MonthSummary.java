cca3a54f200f07742b5406edd278a8098d96c670
Mishanya <ribacokmisa@gmail.com>
2026-08-14 15:11:51 +0300

    feat: validation on month

--- trainer-workload-service/src/main/java/com/epam/gym/workload/dto/response/MonthSummary.java
+++ trainer-workload-service/src/main/java/com/epam/gym/workload/dto/response/MonthSummary.java
@@ -1,9 +1,13 @@
 package com.epam.gym.workload.dto.response;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class MonthSummary {
+    @Min(1)
+    @Max(12)
     private String month;
     private int trainingSummaryDuration;
 }
